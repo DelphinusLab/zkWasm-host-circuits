dbe0b197545509eb52718638369896c76baae024
Fangzhou Yu <fangzhou.yu.25@dartmouth.edu>
2023-06-25 20:32:03 -0700

    babyjub addition test and scalar mul impl

--- src/circuits/babyjub.rs
+++ src/circuits/babyjub.rs
@@ -1,11 +1,12 @@
 use crate::utils::{
     field_to_bn,
     bn_to_field,
+    field_to_u64,
 };
 
 use crate::circuits::{
     CommonGateConfig,
-    Limb,
+    Limb, babyjub, 
 };
 
 use crate::circuits::range::{
@@ -14,7 +15,9 @@ use crate::circuits::range::{
 };
 
 use std::ops::{Mul, Div};
-
+use halo2_proofs::pairing::bn256::Fr;
+use halo2_proofs::circuit::layouter;
+use halo2_proofs::plonk::*;
 use halo2_proofs::{
     arithmetic::FieldExt,
     circuit::{Chip, Region},
@@ -92,7 +95,7 @@ impl<F: FieldExt> BabyJubChip<F> {
             ],
             [None, None, None, None, Some(-F::one()), None, Some(F::one()), None, None],
             0
-        )?[4].clone();
+        )?[4].clone();  // verify x1x2 is correct
         let lambda2 = self.config.assign_line(region, range_check_chip, offset,
             [
                 Some(lhs.y.clone()),
@@ -104,7 +107,7 @@ impl<F: FieldExt> BabyJubChip<F> {
             ],
             [None, None, None, None, Some(-F::one()), None, Some(F::one()), None, None],
             0
-        )?[4].clone();
+        )?[4].clone();  // verify y1y2 is correct
         let lambda = self.config.assign_line(region, range_check_chip, offset,
             [
                 Some(lambda1),
@@ -116,7 +119,7 @@ impl<F: FieldExt> BabyJubChip<F> {
             ],
             [None, None, None, None, Some(-F::one()), None, Some(F::one()), None, None],
             0
-        )?[4].clone();
+        )?[4].clone();  // lambda1*lambda2 = y1y2 * x1x2
 
         let x3_f = lhs.x.value * rhs.y.value + lhs.y.value * rhs.x.value;
         let x3s = self.config.assign_line(region, range_check_chip, offset,
@@ -130,7 +133,7 @@ impl<F: FieldExt> BabyJubChip<F> {
             ],
             [None, None, None, None, Some(F::one()), None, Some(F::one()), Some(F::one()), None],
             0
-        )?[4].clone();
+        )?[4].clone();   // gives limb of x3_f
 
         //x3 * (1+lambda) = x3s
         let x3_f = x3s.value * (F::one() + lambda.value).invert().unwrap();
@@ -145,7 +148,7 @@ impl<F: FieldExt> BabyJubChip<F> {
             ],
             [Some(F::one()), Some(-F::one()), None, None, None, None, Some(F::one()), None, None],
             0
-        )?[3].clone();
+        )?[3].clone();  // this might give lambda
 
 
 
@@ -188,8 +191,382 @@ impl<F: FieldExt> BabyJubChip<F> {
         lhs: &Limb<F>,
         rhs: &Point<F>,
     ) -> Result<Point<F>, Error> {
-        todo!()
+        // parse the lhs to binary format
+        // ret = O, additive identity on babyjub
+        // for each bit {
+        //     add(ret,ret)
+        //     if bit == 1 {
+        //         add(ret,P)
+        //     }
+        // }
+        // Ok(ret)
+        let mut scalar_bin: Vec<Limb<F>> = vec![];
+        self.config.decompose_limb(region, range_check_chip, offset, lhs, &mut scalar_bin, 4)?;   
+        // get the additive identity point
+        let iden_ele = Point{x: Limb::new(None, F::zero()), y: Limb::new(None, F::one())};
+        // constrain it by adding to itself must still be itself
+        let iden_ele_plus_iden_ele = self.add(region, range_check_chip, offset, &mut iden_ele.clone(), &mut iden_ele.clone())?;   
+        let e_x = self.config.assign_line(region, range_check_chip, offset,
+            [
+                None,
+                Some(iden_ele_plus_iden_ele.x.clone()),
+                None,
+                Some(iden_ele.x.clone()),
+                None,
+                None,
+            ],
+            [None, Some(F::one()), None, Some(-F::one()), None, None, None, None, None],
+            0
+        )?[3].clone();
+
+        let e_y = self.config.assign_line(region, range_check_chip, offset,
+            [
+                Some(iden_ele_plus_iden_ele.y.clone()),
+                None,
+                Some(iden_ele.y.clone()),
+                None,
+                None,
+                None,
+            ],
+            [Some(F::one()), None, Some(-F::one()), None, None, None, None, None, None],
+            0
+        )?[2].clone();
+
+        // start with additive identity
+        let ret = Point { x: e_x.clone(), y: e_y.clone()};
+
+        // loop through the decomposed limbs
+        for limb in scalar_bin {    // each limb has val either one or zero
+            // add to itself everytime
+            let ret = self.add(region, range_check_chip, offset, &ret.clone(), &ret.clone())?;  
+            // add the original point iff the limb val is 1
+            let add_x = self.config.select(region, range_check_chip, offset, &limb, &rhs.clone().x, &e_x.clone())?;
+            let add_y = self.config.select(region, range_check_chip, offset, &limb, &rhs.clone().y, &e_y.clone())?;
+            // point to add will be the additive identity if current digit is 0, will be original point o.w
+            let point_to_add = Point{x: add_x, y: add_y};
+            let ret = self.add(region, range_check_chip, offset, &ret.clone(), &point_to_add)?;
+        } 
+        Ok(ret)
+
+        
+
     }
 
 }
 
+#[cfg(test)]
+mod tests {
+    use halo2_proofs::pairing::bn256::Fr;
+    use babyjubjub_rs::*;
+    use ff::*;
+    use futures::future::Inspect;
+    use poseidon_rs::Poseidon;
+    pub type PosFr = poseidon_rs::Fr; // alias
+    
+    use halo2_proofs::dev::MockProver;
+    use num_bigint::BigUint;
+    use halo2_proofs::circuit::layouter;
+    use crate::circuits::range::{
+        RangeCheckConfig,
+        RangeCheckChip,
+    };
+    use crate::value_for_assign;
+    use crate::circuits::{CommonGateConfig, Layouter, SimpleFloorPlanner};
+
+    use halo2_proofs::{
+        circuit::{Chip, Layouter, Region, SimpleFloorPlanner},
+        plonk::{
+            Advice, Circuit, Column, Instance, ConstraintSystem, Error
+        },
+    };
+
+    use super::{
+        BabyJubChip,
+        Point,
+        Limb,
+    };
+
+    #[derive(Debug)]
+    pub struct HelperChipConfig {
+        ret_x: Column<Advice>,
+        ret_y: Column<Advice>,
+        p1_x: Column<Instance>,
+        p1_y: Column<Instance>,
+        p2_x: Column<Instance>,
+        p2_y: Column<Instance>,
+        known_x: Column<Instance>,
+        known_y: Column<Instance>, 
+    }
+
+    #[derive(Debug)]
+    pub struct HelperChip {
+        config: HelperChipConfig
+    }
+
+    impl Chip<Fr> for HelperChip {
+        type Config = HelperChipConfig;
+        type Loaded = ();
+
+        fn config(&self) -> &Self::Config {
+            &self.config
+        }
+
+        fn loaded(&self) -> &Self::Loaded {
+            &()
+        }
+    }
+
+    impl HelperChip {
+        fn new(config: HelperChipConfig) -> Self {
+            HelperChip{
+                config,
+            }
+        }
+
+        fn configure(cs: &mut ConstraintSystem<Fr>,
+            p1_x: Column<Instance>,
+            p1_y: Column<Instance>,
+            p2_x: Column<Instance>,
+            p2_y: Column<Instance>,
+            known_x: Column<Instance>,
+            known_y: Column<Instance>,
+        ) -> HelperChipConfig {
+            let ret_x= cs.advice_column();
+            let ret_y= cs.advice_column();
+
+            cs.enable_equality(ret_x);
+            cs.enable_equality(ret_y);
+            cs.enable_equality(known_x);
+            cs.enable_equality(known_y);
+            HelperChipConfig {
+                ret_x,
+                ret_y,
+                p1_x,
+                p1_y,
+                p2_x,
+                p2_y,
+                known_x,
+                known_y,
+            }
+        }
+
+
+        fn assign_point_to_col(
+            &self,
+            region: &mut Region<Fr>,
+            offset: &mut usize,
+            p: Point<Fr>,
+        ) -> Result<Number<Fr>, Error> {
+            layouter.assign_region(
+                || "na",
+                |mut region| {
+                    self.config.s.enable(&mut region, 0)?;
+                    let x = region.assign_advice(
+                        || "ret_x",
+                        self.config.ret_x,
+                        0, // rotation
+                        p.x.value,
+                    )?;
+                    
+                    let y = region.assign_advice(
+                        || "ret_y",
+                        self.config.ret_y,
+                        0, 
+                        p.y.value,
+                    )?;
+    
+                    Ok((x, y))
+                },
+            )
+        }
+
+        fn assign_p1(&self,
+            region: &mut Region<Fr>,
+            offset: &mut usize,
+            value_x: &mut PosFr,
+            value_y: &mut PosFr,
+        ) -> Result<Point<Fr>, Error> {
+            let cell_1 = region.assign_advice(
+                || format!("p1 x"),
+                self.config.p1_x,
+                *offset,
+                || value_x
+            )?;
+            let cell_2 = region.assign_advice(
+                || format!("p1 y"),
+                self.config.p1_y,
+                *offset,
+                || value_y
+            )?;
+            Ok(Point { x: Limb::new(Some(cell_1).clone(), value_x), y: Limb::new(Some(cell_2).clone(), value_y)})
+        }
+
+        fn assign_p2(&self,
+            region: &mut Region<Fr>,
+            offset: &mut usize,
+            value_x: &mut PosFr,
+            value_y: &mut PosFr,
+        ) -> Result<Point<Fr>, Error> {
+            let cell_1 = region.assign_advice(
+                || format!("p1 x"),
+                self.config.p2_x,
+                *offset,
+                || value_x
+            )?;
+            let cell_2 = region.assign_advice(
+                || format!("p1 y"),
+                self.config.p2_y,
+                *offset,
+                || value_yn 
+            )?;
+            Ok(Point { x: Limb::new(Some(cell_1).clone(), value_x), y: Limb::new(Some(cell_2).clone(), value_y)})
+        }
+
+        fn get_p3(
+            &self,
+            region: &mut Region<Fr>,
+            offset: &mut usize,
+            value_x: &mut PosFr,
+            value_y: &mut PosFr,
+        ) -> Result<(Limb<F>, Limb<F>), Error> {
+            let cell_1 = region.assign_instance(
+                || format!("p1 x"),
+                self.config.known_x,
+                *offset,
+                || value_x,
+                0
+            )?;
+            let cell_2 = region.assign_instance(
+                || format!("p1 y"),
+                self.config.known_y,
+                *offset,
+                || value_y,
+                0
+            )?;
+            Ok( (Limb::new(Some(cell_1).clone(), value_x), Limb::new(Some(cell_2).clone(), value_y)))
+        }
+
+    }
+
+    struct TestCircuit {
+        known_x: PosFr,
+        known_y: PosFr,
+        exp: BigUint,
+        modulus: BigUint,
+    }
+
+    #[derive(Clone, Debug)]
+    struct TestConfig {
+        babyjubconfig: CommonGateConfig,
+        helperconfig: HelperChipConfig,
+        rangecheckconfig: RangeCheckConfig,
+    }
+
+
+
+    struct TestCircuit {
+        p1_x: PosFr,
+        p1_y: PosFr,
+        p2_x: PosFr,
+        p2_y: PosFr,
+        known_x: PosFr,
+        known_y: PosFr, 
+
+    }
+
+    struct TestConfig {
+        babyjubconfig: CommonGateConfig,
+        helperconfig: HelperChipConfig,
+        rangecheckconfig: RangeCheckConfig,
+    }
+
+    impl Circuit<Fr> for TestCircuit {
+        type Config = TestConfig;
+        type FloorPlanner = SimpleFloorPlanner;
+
+        fn without_witnesses(&self) -> Self {
+            Self::default()
+        }
+
+        fn configure(meta: &mut ConstraintSystem<Fr>,
+            p1_x: Column<Instance>,
+            p1_y: Column<Instance>,
+            p2_x: Column<Instance>,
+            p2_y: Column<Instance>,
+        ) -> Self::Config {
+            let rangecheckconfig = RangeCheckChip::<Fr>::configure(meta);
+            Self::Config {
+                babyjubconfig: BabyJubChip::<Fr>::configure(meta, &rangecheckconfig),
+                helperconfig: HelperChip::configure(meta, p1_x, p1_y, p2_x, p2_y),
+                rangecheckconfig,
+            }
+        }
+
+        fn synthesize(
+            &self,
+            config: Self::Config,
+            mut layouter: impl Layouter<Fr>,
+        ) -> Result<(), Error> {
+            let babyjubchip = BabyJubChip::<Fr>::new(config.clone().babyjubconfig);
+            let helperchip = HelperChip::new(config.clone().helperconfig);
+            let mut range_chip = RangeCheckChip::<Fr>::new(config.clone().rangecheckconfig);
+            layouter.assign_region(
+            || "test addition",
+            |mut region| {
+                range_chip.initialize(&mut region)?;
+                let mut offset = 0;
+                // point 1
+                let p1 = helperchip.assign_p1(&mut region, &mut offset, self.p1_x, self.p1_y)?;
+                let p2 = helperchip.assign_p2(&mut region, &mut offset, self.p2_x, self.p2_y)?;
+                let p3 = babyjubchip.add(&mut region, &mut range_chip, &mut offset, & p1, &p2)?;
+                let (x,y) = helperchip.assign_point_to_col(&mut region, &mut offset, & p3);
+                let (fixed_x, fixed_y) = helperchip.get_p3(&mut region, &mut offset, self.known_x, self.known_y)?;
+                region.constrain_equal(x.clone().cell.unwrap().cell(), fixed_x.clone().cell.unwrap().cell())?;
+                region.constrain_equal(y.clone().cell.unwrap().cell(), fixed_y.clone().cell.unwrap().cell())?;
+
+                Ok(())
+            })
+        }
+    }
+
+    #[test]
+    fn test_circuit() {
+        let p1_x =  PosFr::from_str(
+            "16540640123574156134436876038791482806971768689494387082833631921987005038935",
+        )
+        .unwrap();
+        let p1_y = PosFr::from_str(
+            "20819045374670962167435360035096875258406992893633759881276124905556507972311",
+        )
+        .unwrap(); 
+
+        let p2_x =  PosFr::from_str(
+            "16540640123574156134436876038791482806971768689494387082833631921987005038935",
+        )
+        .unwrap();
+        let p2_y = PosFr::from_str(
+            "20819045374670962167435360035096875258406992893633759881276124905556507972311",
+        )
+        .unwrap();
+
+
+        let known_x =  PosFr::from_str(
+            "17777552123799933955779906779655732241715742912184938656739573121738514868268",
+        )
+        .unwrap();
+        let known_y = PosFr::from_str(
+            "2626589144620713026669568689430873010625803728049924121243784502389097019475",
+        )
+        .unwrap();
+
+        let test_circuit = TestCircuit {
+            p1_x,
+            p1_y,
+            p2_x,
+            p2_y,
+            known_x, 
+            known_y} ;
+        let prover = MockProver::run(18, &test_circuit, vec![]).unwrap();
+        assert_eq!(prover.verify(), Ok(()));
+    }
+}

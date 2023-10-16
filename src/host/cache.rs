5dc8b92c2ee6dbe41a7f1e65a397d8f222f83ac4
evan.han <iloversevan@gmail.com>
2023-10-16 16:05:45 +0800

    use env to config merkle cache size

--- src/host/cache.rs
+++ src/host/cache.rs
@@ -8,14 +8,23 @@ use std::sync::Mutex;
 // The LRU feature performs best when maxsize is a power-of-two.
 const DEFAULT_CACHE_SIZE: usize = usize::pow(2, 24);
 
+const ENV_CACHE_SIZE: &str = "ZKWASM_MERKLE_CACHE_SIZE";
+
 lazy_static::lazy_static! {
     pub static ref MERKLE_CACHE: Mutex<LruCache<(u64, [u8;  32]), Option<MerkleRecord>>> =
         Mutex::new(LruCache::<(u64, [u8;  32]), Option<MerkleRecord>>::new(
-            NonZeroUsize::new(DEFAULT_CACHE_SIZE).unwrap(),
+            NonZeroUsize::new(get_cache_size()).unwrap(),
         ));
 
     pub static ref DATA_CACHE: Mutex<LruCache<[u8; 32], Option<DataHashRecord>>> =
     Mutex::new(LruCache::<[u8; 32], Option<DataHashRecord>>::new(
-        NonZeroUsize::new(DEFAULT_CACHE_SIZE).unwrap(),
+        NonZeroUsize::new(get_cache_size()).unwrap(),
     ));
+}
+
+fn get_cache_size() -> usize {
+    if let Ok(size_var) = std::env::var(ENV_CACHE_SIZE) {
+        return size_var.parse::<usize>().unwrap_or(DEFAULT_CACHE_SIZE);
+    }
+    DEFAULT_CACHE_SIZE
 }

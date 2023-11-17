d2f71dc0c62dac438104e817c84513741450f6c6
Sinka <xgao@zoyoe.com>
2023-11-17 21:52:28 +0800

    add profile of offset

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 cargo test generate_keccak_input_multi
+RUST_BACKTRACE=1 cargo run --release -- --input keccak256_test.json --opname keccakhash --output output --param params
 RUST_BACKTRACE=1 cargo run --release -- --input keccak256_test_multi.json --opname keccakhash --output output --param params

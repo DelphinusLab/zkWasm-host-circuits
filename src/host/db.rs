7587040f0d83e040bd503a14346d49ea7c899e19
qozymandias <odowning19@gmail.com>
2025-03-27 15:03:01 +1100

    fix

--- src/host/db.rs
+++ src/host/db.rs
@@ -340,12 +340,12 @@ impl RocksDB {
             .db
             .cf_handle(&self.merkle_cf_name)
             .ok_or_else(|| anyhow::anyhow!("Merkle column family not found"))?;
-        self.db.compact_range_cf(merkle_cf, None, None);
+        self.db.compact_range_cf(merkle_cf, None::<&[u8]>, None::<&[u8]>);
         let data_cf = self
             .db
             .cf_handle(&self.data_cf_name)
             .ok_or_else(|| anyhow::anyhow!("Merkle column family not found"))?;
-        self.db.compact_range_cf(data_cf, None, None);
+        self.db.compact_range_cf(data_cf, None::<&[u8]>, None::<&[u8]>);
         Ok(())
     }
 }
